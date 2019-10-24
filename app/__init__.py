be2711a2f5b6f3b4af1a600703ea21145a1177aa
german.simomcini <german.simoncini@ubiome.com>
2019-10-24 11:13:44 -0300

    Changes to run app in heroku

--- app/__init__.py
+++ app/__init__.py
@@ -11,5 +11,14 @@ mongo_client = MongoClient()
 mongo_client = MongoClient(config.MONGO_URI, connect=False)
 mongo_db = mongo_client[config.MONGO_DATABASE]
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 from app.minesweeper.resource import *
+
+
+
+if __name__ == '__main__':
+    app.run()
 
